bffecba4186da3a9188dc6ed84b5505d4445c2a0
Nicola <nicola.porta00@gmail.com>
2021-02-02 15:47:29 +0100

    Delete background in web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('home_page', [
         "word1" => "HELLO",
         "word2" => "WORLD",
-        "tests" => ["H", "E", "L", "L", "O", " ", "W", "O", "R", "L", "D", "!", "!", "!"],
-        "background" => "https://lh3.googleusercontent.com/proxy/C2nGEyG8OzJTvfL77IDtIONcCo3bC6Sq8Ug_enoxdFPCAEcz9QBR5CKZgwJ-QxrBp9PCcSQkVYxdUvckXGxXHA040cvn8T0D3fWnVtb9Z9vF74PpwGg_"
+        "tests" => ["H", "E", "L", "L", "O", " ", "W", "O", "R", "L", "D", "!", "!", "!"]
     ]);
 });
